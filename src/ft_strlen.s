c78652d59aa3be04d41d6b9186b52d9a0a1543fe
akabab <ycribier@student.42.fr>
2016-03-06 17:27:52 +0100

    opti strlen

--- src/ft_strlen.s
+++ src/ft_strlen.s
@@ -2,14 +2,15 @@ segment .text
 	global		_ft_strlen
 
 _ft_strlen:
-	mov		rax, 0
-
-loop:
-	cmp byte	[rdi], 0
-	je			done
-	inc			rdi
-	inc			rax
-	jmp			loop
+	push		rdi
+	sub			rcx, rcx
+	sub			al, al
+	not			rcx
+	cld
+	repne		scasb
+	not			rcx
+	pop			rdi
+	lea			rax, [rcx-1]
 
 done:
 	ret
